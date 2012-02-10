547698ac3bfbd644b17061a34b0747fc9a58a245
Luis Pablo Gallo <luispablo.gallo@gmail.com>
2012-02-10 19:02:23 -0300

    Creación del proyecto.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AltaAgenda::Application.config.secret_token = '2c99e7680110ea155477f73dfd0ebe0b28a9253fe45cba00198915e0aa41359545b8da34e85f18ab7978210b03d1099d32da397d916ff0b1a9f02d6f949b01a1'
