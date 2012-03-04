6cf478588c54735ac6fdb28b918a40ce0b859840
Ariel Juszczyk <ariel.juszczyk@gmail.com>
2012-03-04 19:27:13 -0300

    Filtros para eventos de la home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,13 +2,29 @@ class HomeController < AdminController
   # GET /eventos
   # GET /eventos.json
   def index
-    @eventos = Evento.all
+    
+   @eventos = Evento.all(:order => 'fecha ASC')   
+   @orden = 'fecha ASC'
+ 
+    case params[:id_fecha]
+    when "1"  #Hoy
+      @DateFilter = Date.today.to_s + " 00:00:00.000000"      
+      @eventos = Evento.where(:fecha => @DateFilter).all(:order => @orden)   
+    when "2"  #Mañana          
+      @DateFilter = Date.tomorrow.to_s + " 00:00:00.000000"  
+      @eventos = Evento.where(:fecha => @DateFilter).all(:order => @orden)     
+    when "3"  #Fin de semana      
+      @eventos = Evento.where(:fecha => Date.today.beginning_of_week+5..Date.today.next_week.at_beginning_of_week).all(:order => @orden)         
+    when "4"  #Esta semana         
+      @eventos = Evento.where(:fecha => Date.today.beginning_of_week..Date.today.end_of_week).all(:order => @orden)   
+    when "5"  #Este mes
+      @eventos = Evento.where(:fecha => Date.today.beginning_of_month..Date.today.end_of_month).all(:order => @orden)   
+    end    
 
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @eventos }
     end
   end
-
   
 end
