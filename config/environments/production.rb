1e1327b53df7a5566fb9933ea63c9cc9234a439b
Luis Pablo Gallo <luispablo.gallo@gmail.com>
2012-03-19 23:53:53 -0300

    Agregar bootstrap al precompilador.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,8 @@
 AltaAgenda::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
+  config.assets.precompile += %w( bootstrap.css )
+  
   # Code is not reloaded between requests
   config.cache_classes = true
 
