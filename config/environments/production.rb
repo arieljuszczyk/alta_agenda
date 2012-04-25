e476130534c84711ee8f70fc92cfc4002634e94b
Luis Pablo <luispablo.gallo@gmail.com>
2012-04-24 21:06:20 -0300

    Precompilar css de importaciones.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 AltaAgenda::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
-  config.assets.precompile += %w( bootstrap.css bootstrap.min.css bootstrap.js bootstrap.min.js )
+  config.assets.precompile += %w( bootstrap.css bootstrap.min.css bootstrap.js bootstrap.min.js importaciones.css )
   
   # Code is not reloaded between requests
   config.cache_classes = true
